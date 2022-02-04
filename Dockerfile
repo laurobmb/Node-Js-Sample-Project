c1874c1c01963254969f9eb71415461be4d8392d
Lauro Gomes <lagomes@redhat.com>
2022-02-04 18:01:59 -0300

    Nova pagina

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,7 @@
-#Choose a base image
 FROM node
-
-# Create a new folder for our application
-RUN mkdir -p /usr/src/app
-
-# Set the working dir when our container executes
 WORKDIR /usr/src/app
-
-# Install app dependencies
-COPY package.json .
-# For npm@5 or later, copy package-lock.json as well
-# COPY package.json package-lock.json ./
-
-# Install our packages
+COPY [ "server.js","index.html","package.json","./" ]
 RUN npm install
-
-# Copy the rest of our application and Bundle app source
-COPY . /usr/src/app
-
-#Expose our application port
+ONBUILD COPY . /usr/src/app
 EXPOSE 8080
-
-# Set start command
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict';
-
+const http = require('http');
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +8,19 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+
 app.get('/', (req, res) => {
   res.send('Teste para Opeshift 4.8\n');
 });
+
+app.use('/page1', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+const server = http.createServer(app);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
