41166cf3bf1d5629202aa0b29fd274bb75717c9e
Lauro Gomes <lagomes@redhat.com>
2022-04-28 23:18:45 -0300

    Ajustanto code

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ app.use(express.json());
 app.use(express.static("express"));
 app.use('/img', express.static('img'));
 
+// router1
 app.get('/openshift', (req, res) => {
   res.send('Teste para Opeshift 4.8\n');
 });
 
+// upload files
 app.use('/files', express.static('data/'), 
 serveIndex('data/', {'icons': true}))
 
+// health server
 app.get('/health', (req, res) => {
   const data = {
     uptime: process.uptime(),
@@ -29,6 +32,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('Ok');
 });
 
+// root
 app.use('/', function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
